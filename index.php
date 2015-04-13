9e520202f970ee3ab874738c1fb71aeeb1da4dfa
TanguyS <tanguysauvin@gmail.com>
2015-04-13 15:14:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
-<?php
-// there nothing here
-?>
+<?php get_part('header'); ?>
+
+<div id="wpng-app" class="main-container" ng-app="wpng">
+	<div ui-view="container"></div>
+</div>
+<?php get_part('footer'); ?>
